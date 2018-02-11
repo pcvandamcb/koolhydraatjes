765abbb3b669fe58cedd89ec97f69fd77044543c
Lauriëtte de Ruiter <info@lauriette.com>
2018-02-11 18:15:05 +0100

    Add styling detail view

--- src/components/ProductDetails.js
+++ src/components/ProductDetails.js
@@ -6,24 +6,27 @@ const ProductDetails = ({ match }) => {
   const productDetails = products.find(product => product.id === parseInt(match.params.id, 10));
 
   return (
-    <div>
+    <div className="product-detail">
       <h1>
         {productDetails.name}, {productDetails.brand}
       </h1>
       <ul>
         <li>
           <h2>Voedingswaarde</h2>
-          100gram <span className="kh100gr">{productDetails.kh100gr}</span>
+          100gram <span className="kh100gr">{productDetails.kh100gr} KH</span>
         </li>
         <li>
           <h2>Portie</h2>
           {productDetails.serving}gr
           <span className="kh100gr">
-            {(productDetails.kh100gr / 100 * productDetails.serving).toLocaleString('nl-NL')}
+            {(productDetails.kh100gr / 100 * productDetails.serving).toLocaleString('nl-NL')} KH
           </span>
         </li>
       </ul>
-      <p>{productDetails.notes}</p>
+      <div className="product-notes">
+        <h2>Notes</h2>
+        <p>{productDetails.notes}</p>
+      </div>
     </div>
   );
 };
