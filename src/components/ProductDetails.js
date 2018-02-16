4371f9a2d0f6e4143b3f47fcfa93a0828dd0ccde
Paul van Dam <p.vandam@coolblue.nl>
2018-02-16 21:57:14 +0100

    Add custom serving option

--- src/components/ProductDetails.js
+++ src/components/ProductDetails.js
@@ -1,55 +1,82 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
 import products from '../data/products';
 
-const ProductDetails = ({ match }) => {
-  const productDetails = products.find(product => product.id === parseInt(match.params.id, 10));
+const calcKH = (kh100gr, serving) =>
+  (Math.round(kh100gr / 100 * parseInt(serving, 10) * 10) / 10).toLocaleString('nl-NL');
 
-  return (
-    <div className="app-wrapper">
-      <div className="navigation-bar">
-        <div className="back-button">
-          <Link to="/">
-            <i className="fas fa-angle-left" />
-            <span>Terug</span>
-          </Link>
+class ProductDetails extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      customServing: ''
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({
+      customServing: event.target.value
+    });
+  }
+
+  render() {
+    const { match } = this.props;
+    const { customServing } = this.state;
+    const productDetails = products.find(product => product.id === parseInt(match.params.id, 10));
+
+    return (
+      <div className="app-wrapper">
+        <div className="navigation-bar">
+          <div className="back-button">
+            <Link to="/">
+              <i className="fas fa-angle-left" />
+              <span>Terug</span>
+            </Link>
+          </div>
         </div>
-      </div>
-      <div className="app-content">
-        <div className="product-detail">
-          <div className="product-name">
-            <h1>{productDetails.name}</h1>
-            <span>{productDetails.brand}</span>
-          </div>
-          <div className="product-details">
-            <ul>
-              <li>
-                <h2>Voedingswaarde</h2>
-                100 gram <span className="kh">{productDetails.kh100gr} KH</span>
-              </li>
-              <li>
-                <h2>Portie</h2>
-                {productDetails.serving} gram
-                <span className="kh">
-                  {(
-                    Math.round(productDetails.kh100gr / 100 * productDetails.serving * 10) / 10
-                  ).toLocaleString('nl-NL')}{' '}
-                  KH
-                </span>
-              </li>
-            </ul>
-          </div>
-          {productDetails.notes && (
-            <div className="product-notes">
-              <h2>Notes</h2>
-              <p>{productDetails.notes}</p>
+        <div className="app-content">
+          <div className="product-detail">
+            <div className="product-name">
+              <h1>{productDetails.name}</h1>
+              <span>{productDetails.brand}</span>
             </div>
-          )}
+            <div className="product-details">
+              <ul>
+                <li>
+                  <h2>Voedingswaarde</h2>
+                  100 gram <span className="kh">{productDetails.kh100gr} KH</span>
+                </li>
+                <li>
+                  <h2>Portie</h2>
+                  {productDetails.serving} gram
+                  <span className="kh">
+                    {calcKH(productDetails.kh100gr, productDetails.serving)} KH
+                  </span>
+                </li>
+                <li>
+                  <h2>Eigen hoeveelheid</h2>
+                  <input type="text" onChange={this.handleChange} /> gram
+                  {parseInt(customServing, 10) > 0 && (
+                    <span className="kh">{calcKH(productDetails.kh100gr, customServing)} KH</span>
+                  )}
+                </li>
+              </ul>
+            </div>
+            {productDetails.notes && (
+              <div className="product-notes">
+                <h2>Notes</h2>
+                <p>{productDetails.notes}</p>
+              </div>
+            )}
+          </div>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default ProductDetails;
